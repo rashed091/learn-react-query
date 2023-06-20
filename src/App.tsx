22f9dd2146efdc6b2450ffb8c23e1d85b195e5dc
mrasheduzzaman <rashed091nsu@gmail.com>
2023-06-20 22:32:44 +0600

    Add react-query and axios package

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "react-query";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const result = useQuery("pokemon", () =>
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => res.data)
+  );
+
+  console.log(result);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {result?.data?.results.map((poke: { name: string }) => (
+        <div key={poke.name}>{poke.name}</div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
